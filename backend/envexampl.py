4534caa1c1ea97452d7dca9fec53c00e5601356b
Serge-Moskalenko <skmos2019@outlook.com>
2025-04-25 20:11:41 +0100

    add Postgres

---
+++ backend/envexampl.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: int
+    DB_HOST: str = "127.0.0.1"
+    DB_PORT: int = 5432
+    DATABASE_URL: str
+
+    SECRET_KEY: str
+    DEBUG: bool = False
+    ALLOWED_HOSTS: list[str] = []
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+settings = Settings()
