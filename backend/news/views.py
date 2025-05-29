16731650f317d13fd22653fad1949bce16e3dbd0
Serge-Moskalenko <skmos2019@outlook.com>
2025-05-29 15:29:58 +0100

    fix news

--- backend/news/views.py
+++ backend/news/views.py
@@ -2,8 +2,11 @@ from rest_framework import viewsets
 
 from .models import Article
 from .serializers import NewsItemSerializer
+from rest_framework.permissions import AllowAny
 
 
 class NewsItemViewSet(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = []
+    permission_classes = [AllowAny]
     queryset = Article.objects.all()
     serializer_class = NewsItemSerializer
