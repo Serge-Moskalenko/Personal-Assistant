a3557f8e3102c1d5309c44b16660854c90f74d4d
Serge-Moskalenko <skmos2019@outlook.com>
2025-05-29 19:52:19 +0100

    fix guard

--- frontend/src/app/auth/hooks/useAuthGuard.ts
+++ frontend/src/app/auth/hooks/useAuthGuard.ts
@@ -1,18 +1,19 @@
 "use client";
-
+import { useAuthStore } from "@/stores/useAuthStore";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { useAuthStore } from "@/stores/useAuthStore";
 
 export function useAuthGuard() {
   const router = useRouter();
   const { accessToken, clearTokens } = useAuthStore();
+  const hasHydrated = useAuthStore.persist.hasHydrated;
 
   useEffect(() => {
+    if (!hasHydrated) return;
+
     if (accessToken === null) {
       clearTokens();
       router.replace("/login");
     }
-  }, [accessToken]);
-
+  }, [hasHydrated, accessToken]);
 }
